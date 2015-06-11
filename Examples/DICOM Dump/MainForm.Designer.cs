8ec5a8da95f9215b8c0fa186ce17b86566a58770
Muhammad Aboulmagd <yamaha.otaka@gmail.com>
2015-06-11 14:27:25 +0200

    Adding Examples from different forks
    Adding Examples from different forks to my private fork but it doesn't
    build correctly yet

---
+++ Examples/DICOM Dump/MainForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Dicom.Dump {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuItemTools = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuItemView = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuItemSyntax = new System.Windows.Forms.ToolStripMenuItem();
+			this.explicitVRLittleEndianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.implicitVRLittleEndianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.explicitVRBigEndianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.jPEGLosslessP14SV1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.jPEG2000LosslessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.jPEGLSLosslessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.rLELosslessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.lvDicom = new System.Windows.Forms.ListView();
+			this.columnHeaderTag = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeaderVR = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeaderLength = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeaderValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.cmDicom = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.copyValueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.cmDicom.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.menuItemTools});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(792, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.menuItemSave});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.openToolStripMenuItem.Text = "&Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.OnClickOpen);
+			// 
+			// menuItemSave
+			// 
+			this.menuItemSave.Enabled = false;
+			this.menuItemSave.Name = "menuItemSave";
+			this.menuItemSave.Size = new System.Drawing.Size(152, 22);
+			this.menuItemSave.Text = "&Save";
+			this.menuItemSave.Click += new System.EventHandler(this.OnClickSave);
+			// 
+			// menuItemTools
+			// 
+			this.menuItemTools.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemView,
+            this.menuItemSyntax});
+			this.menuItemTools.Name = "menuItemTools";
+			this.menuItemTools.Size = new System.Drawing.Size(44, 20);
+			this.menuItemTools.Text = "&Tools";
+			// 
+			// menuItemView
+			// 
+			this.menuItemView.Enabled = false;
+			this.menuItemView.Name = "menuItemView";
+			this.menuItemView.Size = new System.Drawing.Size(152, 22);
+			this.menuItemView.Text = "&View";
+			this.menuItemView.Click += new System.EventHandler(this.OnClickView);
+			// 
+			// menuItemSyntax
+			// 
+			this.menuItemSyntax.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.explicitVRLittleEndianToolStripMenuItem,
+            this.implicitVRLittleEndianToolStripMenuItem,
+            this.explicitVRBigEndianToolStripMenuItem,
+            this.jPEGLosslessP14SV1ToolStripMenuItem,
+            this.jPEG2000LosslessToolStripMenuItem,
+            this.jPEGLSLosslessToolStripMenuItem,
+            this.rLELosslessToolStripMenuItem});
+			this.menuItemSyntax.Enabled = false;
+			this.menuItemSyntax.Name = "menuItemSyntax";
+			this.menuItemSyntax.Size = new System.Drawing.Size(152, 22);
+			this.menuItemSyntax.Text = "&Change Syntax";
+			// 
+			// explicitVRLittleEndianToolStripMenuItem
+			// 
+			this.explicitVRLittleEndianToolStripMenuItem.Name = "explicitVRLittleEndianToolStripMenuItem";
+			this.explicitVRLittleEndianToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.explicitVRLittleEndianToolStripMenuItem.Text = "Explicit VR Little Endian";
+			this.explicitVRLittleEndianToolStripMenuItem.Click += new System.EventHandler(this.OnClickExplicitVRLittleEndian);
+			// 
+			// implicitVRLittleEndianToolStripMenuItem
+			// 
+			this.implicitVRLittleEndianToolStripMenuItem.Name = "implicitVRLittleEndianToolStripMenuItem";
+			this.implicitVRLittleEndianToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.implicitVRLittleEndianToolStripMenuItem.Text = "Implicit VR Little Endian";
+			this.implicitVRLittleEndianToolStripMenuItem.Click += new System.EventHandler(this.OnClickImplicitVRLittleEndian);
+			// 
+			// explicitVRBigEndianToolStripMenuItem
+			// 
+			this.explicitVRBigEndianToolStripMenuItem.Name = "explicitVRBigEndianToolStripMenuItem";
+			this.explicitVRBigEndianToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.explicitVRBigEndianToolStripMenuItem.Text = "Explicit VR Big Endian";
+			this.explicitVRBigEndianToolStripMenuItem.Click += new System.EventHandler(this.OnClickExplicitVRBigEndian);
+			// 
+			// jPEGLosslessP14SV1ToolStripMenuItem
+			// 
+			this.jPEGLosslessP14SV1ToolStripMenuItem.Name = "jPEGLosslessP14SV1ToolStripMenuItem";
+			this.jPEGLosslessP14SV1ToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.jPEGLosslessP14SV1ToolStripMenuItem.Text = "JPEG Lossless P14 SV1";
+			this.jPEGLosslessP14SV1ToolStripMenuItem.Click += new System.EventHandler(this.OnClickJPEGLosslessP14SV1);
+			// 
+			// jPEG2000LosslessToolStripMenuItem
+			// 
+			this.jPEG2000LosslessToolStripMenuItem.Name = "jPEG2000LosslessToolStripMenuItem";
+			this.jPEG2000LosslessToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.jPEG2000LosslessToolStripMenuItem.Text = "JPEG 2000 Lossless";
+			this.jPEG2000LosslessToolStripMenuItem.Click += new System.EventHandler(this.OnClickJPEG2000Lossless);
+			// 
+			// jPEGLSLosslessToolStripMenuItem
+			// 
+			this.jPEGLSLosslessToolStripMenuItem.Name = "jPEGLSLosslessToolStripMenuItem";
+			this.jPEGLSLosslessToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.jPEGLSLosslessToolStripMenuItem.Text = "JPEG-LS Lossless";
+			this.jPEGLSLosslessToolStripMenuItem.Click += new System.EventHandler(this.OnClickJPEGLSLossless);
+			// 
+			// rLELosslessToolStripMenuItem
+			// 
+			this.rLELosslessToolStripMenuItem.Name = "rLELosslessToolStripMenuItem";
+			this.rLELosslessToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+			this.rLELosslessToolStripMenuItem.Text = "RLE Lossless";
+			this.rLELosslessToolStripMenuItem.Click += new System.EventHandler(this.OnClickRLELossless);
+			// 
+			// lvDicom
+			// 
+			this.lvDicom.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderTag,
+            this.columnHeaderVR,
+            this.columnHeaderLength,
+            this.columnHeaderValue});
+			this.lvDicom.ContextMenuStrip = this.cmDicom;
+			this.lvDicom.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lvDicom.FullRowSelect = true;
+			this.lvDicom.Location = new System.Drawing.Point(0, 24);
+			this.lvDicom.MultiSelect = false;
+			this.lvDicom.Name = "lvDicom";
+			this.lvDicom.Size = new System.Drawing.Size(792, 549);
+			this.lvDicom.TabIndex = 1;
+			this.lvDicom.UseCompatibleStateImageBehavior = false;
+			this.lvDicom.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeaderTag
+			// 
+			this.columnHeaderTag.Text = "Tag";
+			this.columnHeaderTag.Width = 230;
+			// 
+			// columnHeaderVR
+			// 
+			this.columnHeaderVR.Text = "VR";
+			this.columnHeaderVR.Width = 40;
+			// 
+			// columnHeaderLength
+			// 
+			this.columnHeaderLength.Text = "Length";
+			// 
+			// columnHeaderValue
+			// 
+			this.columnHeaderValue.Text = "Value";
+			this.columnHeaderValue.Width = 440;
+			// 
+			// cmDicom
+			// 
+			this.cmDicom.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyValueToolStripMenuItem});
+			this.cmDicom.Name = "cmDicom";
+			this.cmDicom.Size = new System.Drawing.Size(129, 26);
+			this.cmDicom.Opening += new System.ComponentModel.CancelEventHandler(this.OnContextMenuOpening);
+			// 
+			// copyValueToolStripMenuItem
+			// 
+			this.copyValueToolStripMenuItem.Name = "copyValueToolStripMenuItem";
+			this.copyValueToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+			this.copyValueToolStripMenuItem.Text = "&Copy Value";
+			this.copyValueToolStripMenuItem.Click += new System.EventHandler(this.OnClickContextMenuCopyValue);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(792, 573);
+			this.Controls.Add(this.lvDicom);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "DICOM Dump";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.cmDicom.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ListView lvDicom;
+		private System.Windows.Forms.ColumnHeader columnHeaderTag;
+		private System.Windows.Forms.ColumnHeader columnHeaderVR;
+		private System.Windows.Forms.ColumnHeader columnHeaderLength;
+		private System.Windows.Forms.ColumnHeader columnHeaderValue;
+		private System.Windows.Forms.ContextMenuStrip cmDicom;
+		private System.Windows.Forms.ToolStripMenuItem copyValueToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem menuItemSave;
+		private System.Windows.Forms.ToolStripMenuItem menuItemTools;
+		private System.Windows.Forms.ToolStripMenuItem menuItemView;
+		private System.Windows.Forms.ToolStripMenuItem menuItemSyntax;
+		private System.Windows.Forms.ToolStripMenuItem jPEGLosslessP14SV1ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem explicitVRLittleEndianToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem implicitVRLittleEndianToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem explicitVRBigEndianToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem jPEG2000LosslessToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem jPEGLSLosslessToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem rLELosslessToolStripMenuItem;
+	}
+}
+
---
+++ Examples/DICOM Dump/MainForm.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Dicom;
+using Dicom.Imaging;
+using Dicom.Imaging.Codec;
+using Dicom.IO.Buffer;
+
+namespace Dicom.Dump {
+	public partial class MainForm : Form {
+		private DicomFile _file;
+
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		private void Reset() {
+			lvDicom.Items.Clear();
+			menuItemView.Enabled = false;
+		}
+
+		private delegate void AddItemDelegate(string tag, string vr, string length, string value);
+
+		private void AddItem(string tag, string vr, string length, string value) {
+			if (InvokeRequired) {
+			    BeginInvoke(new AddItemDelegate(AddItem), tag, vr, length, value);
+			    return;
+			}
+
+			var lvi = lvDicom.Items.Add(tag);
+			lvi.SubItems.Add(vr);
+			lvi.SubItems.Add(length);
+			lvi.SubItems.Add(value);
+		}
+
+		public void OpenFile(string fileName) {
+			DicomFile file = null;
+
+			try {
+				file = DicomFile.Open(fileName);
+			} catch (DicomFileException ex) {
+				file = ex.File;
+				MessageBox.Show(this, "Exception while loading DICOM file: " + ex.Message, "Error loading DICOM file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			OpenFile(file);
+		}
+
+		public void OpenFile(DicomFile file) {
+			try {
+				lvDicom.BeginUpdate();
+
+				Reset();
+
+				_file = file;
+
+				new DicomDatasetWalker(_file.FileMetaInfo).Walk(new DumpWalker(this));
+				new DicomDatasetWalker(_file.Dataset).Walk(new DumpWalker(this));
+
+				if (_file.Dataset.Contains(DicomTag.PixelData))
+					menuItemView.Enabled = true;
+				menuItemSyntax.Enabled = true;
+				menuItemSave.Enabled = true;
+			} catch (Exception ex) {
+				MessageBox.Show(this, "Exception while loading DICOM file: " + ex.Message, "Error loading DICOM file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			} finally {
+				lvDicom.EndUpdate();
+			}
+		}
+
+		private void OnClickOpen(object sender, EventArgs e) {
+			var ofd = new OpenFileDialog();
+			ofd.Filter = "DICOM Files (*.dcm;*.dic)|*.dcm;*.dic|All Files (*.*)|*.*";
+
+			if (ofd.ShowDialog() == DialogResult.Cancel)
+				return;
+
+			OpenFile(ofd.FileName);
+		}
+
+		private void OnClickSave(object sender, EventArgs e) {
+			var sfd = new SaveFileDialog();
+			sfd.Filter = "DICOM (*.dcm)|*.dcm|Image (*.bmp;*.jpg;*.png;*.gif)|*.bmp;*.jpg;*.png;*.gif";
+
+			if (sfd.ShowDialog() == DialogResult.Cancel)
+				return;
+
+			if (sfd.FilterIndex == 1) {
+				var file = new DicomFile(_file.Dataset);
+				file.Save(sfd.FileName);
+			} else {
+				var image = new DicomImage(_file.Dataset);
+				image.RenderImage().Save(sfd.FileName);
+			}
+		}
+
+		private class DumpWalker : IDicomDatasetWalker {
+			int _level = 0;
+
+			public DumpWalker(MainForm form) {
+				Form = form;
+				Level = 0;
+			}
+
+			public MainForm Form {
+				get;
+				set;
+			}
+
+			public int Level {
+				get { return _level; }
+				set {
+					_level = value;
+					Indent = String.Empty;
+					for (int i = 0; i < _level; i++)
+						Indent += "    ";
+				}
+			}
+
+			private string Indent {
+				get;
+				set;
+			}
+
+			public void OnBeginWalk(DicomDatasetWalker walker, DicomDatasetWalkerCallback callback) {
+			}
+
+			public bool OnElement(DicomElement element) {
+				var tag = String.Format("{0}{1}  {2}", Indent, element.Tag.ToString().ToUpper(), element.Tag.DictionaryEntry.Name);
+
+				string value = "<large value not displayed>";
+				if (element.Length <= 2048)
+					value = String.Join("\\", element.Get<string[]>());
+
+				if (element.ValueRepresentation == DicomVR.UI && element.Count > 0) {
+					var uid = element.Get<DicomUID>(0);
+					var name = uid.Name;
+					if (name != "Unknown")
+						value = String.Format("{0} ({1})", value, name);
+				}
+
+				Form.AddItem(tag, 
+					element.ValueRepresentation.Code, 
+					element.Length.ToString(), 
+					value);
+				return true;
+			}
+
+			public bool OnBeginSequence(DicomSequence sequence) {
+				var tag = String.Format("{0}{1}  {2}", Indent, sequence.Tag.ToString().ToUpper(), sequence.Tag.DictionaryEntry.Name);
+
+				Form.AddItem(tag, "SQ", String.Empty, String.Empty);
+
+				Level++;
+				return true;
+			}
+
+			public bool OnBeginSequenceItem(DicomDataset dataset) {
+				var tag = String.Format("{0}Sequence Item:", Indent);
+
+				Form.AddItem(tag, String.Empty, String.Empty, String.Empty);
+
+				Level++;
+				return true;
+			}
+
+			public bool OnEndSequenceItem() {
+				Level--;
+				return true;
+			}
+
+			public bool OnEndSequence() {
+				Level--;
+				return true;
+			}
+
+			public bool OnBeginFragment(DicomFragmentSequence fragment) {
+				var tag = String.Format("{0}{1}  {2}", Indent, fragment.Tag.ToString().ToUpper(), fragment.Tag.DictionaryEntry.Name);
+
+				Form.AddItem(tag, fragment.ValueRepresentation.Code, String.Empty, String.Empty);
+
+				Level++;
+				return true;
+			}
+
+			public bool OnFragmentItem(IByteBuffer item) {
+				var tag = String.Format("{0}Fragment", Indent);
+
+				Form.AddItem(tag, String.Empty, item.Size.ToString(), String.Empty);
+				return true;
+			}
+
+			public bool OnEndFragment() {
+				Level--;
+				return true;
+			}
+
+			public void OnEndWalk() {
+			}
+		}
+
+		private void OnClickView(object sender, EventArgs e) {
+			var form = new DisplayForm(_file);
+			form.ShowDialog(this);
+		}
+
+		private void OnContextMenuOpening(object sender, CancelEventArgs e) {
+			var point = lvDicom.PointToClient(MousePosition);
+
+			var item = lvDicom.GetItemAt(point.X, point.Y);
+			if (item == null) {
+				e.Cancel = true;
+				return;
+			}
+
+			item.Selected = true;
+		}
+
+		private void OnClickContextMenuCopyValue(object sender, EventArgs e) {
+			if (lvDicom.SelectedItems.Count == 0)
+				return;
+
+			var item = lvDicom.SelectedItems[0];
+			var value = item.SubItems[3].Text;
+
+			Clipboard.SetText(value);			
+		}
+
+		private void ChangeSyntax(DicomTransferSyntax syntax) {
+			var file = _file.ChangeTransferSyntax(syntax);
+			OpenFile(file);
+		}
+
+		private void OnClickExplicitVRLittleEndian(object sender, EventArgs e) {
+			ChangeSyntax(DicomTransferSyntax.ExplicitVRLittleEndian);
+		}
+
+		private void OnClickImplicitVRLittleEndian(object sender, EventArgs e) {
+			ChangeSyntax(DicomTransferSyntax.ImplicitVRLittleEndian);
+		}
+
+		private void OnClickExplicitVRBigEndian(object sender, EventArgs e) {
+			ChangeSyntax(DicomTransferSyntax.ExplicitVRBigEndian);
+		}
+
+		private void OnClickJPEGLosslessP14SV1(object sender, EventArgs e) {
+			ChangeSyntax(DicomTransferSyntax.JPEGProcess14SV1);
+		}
+
+		private void OnClickJPEG2000Lossless(object sender, EventArgs e) {
+			ChangeSyntax(DicomTransferSyntax.JPEG2000Lossless);
+		}
+
+		private void OnClickJPEGLSLossless(object sender, EventArgs e) {
+			ChangeSyntax(DicomTransferSyntax.JPEGLSLossless);
+		}
+
+		private void OnClickRLELossless(object sender, EventArgs e) {
+			ChangeSyntax(DicomTransferSyntax.RLELossless);
+		}
+	}
+}
